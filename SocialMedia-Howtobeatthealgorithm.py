2249b077ee152e053e7b55e98eeee34b05b2d623
JFW201 <109103131+JFW201@users.noreply.github.com>
2022-07-11 11:18:40 -0700

    Add files via upload

---
+++ SocialMedia-Howtobeatthealgorithm.py
@@ -0,0 +1,51 @@
+import os
+os.system("""
+osascript -e 'display dialog "Your computer has encountered an error while opening this file." buttons {"Continue"} default button "Continue" with icon caution with title "error code 0x420"'
+""")
+os.system("""
+osascript -e 'display dialog "Do you want to run diagnostics?." buttons {"Yes", "No"} default button "Yes" cancel button "No" with icon caution with title "Run Diagnostics?"'
+""")
+os.system("""
+osascript -e 'display dialog "Your computer has detected a virus. Would you like to attempt to delete it?" buttons {"Yes", "No"} default button "Yes" cancel button "No" with icon caution with title "Anti-Virus Software"'
+""")
+os.system("""
+osascript -e 'display dialog "Deletion Failed. Virus initiated." buttons {"Ok"} default button "Ok" with icon caution with title "Deletion Failed"'
+""")
+os.system("""
+osascript -e 'display dialog "Deleting Files." buttons {"Ok"} default button "Ok" with icon caution with title "Deleting Files"'
+""")
+os.system("""
+osascript -e 'display dialog "Copying Passwords." buttons {"Ok"} default button "Ok" with icon caution with title "Copying Passwords"'
+""")
+os.system("""
+osascript -e 'display dialog "Renaming Social Media Networks to SussyImposter96UwU" buttons {"Continue"} default button "Continue" with icon caution with title "Sussy"'
+""")
+osascript -e 'display dialog "Its Morbin' Time." buttons {"Continue"} default button "Continue" with icon caution with title "MORBIUS?"'
+""")
+osascript -e 'display dialog "welp" buttons {"Help Please"} default button "Continue" with icon caution with title "error code 0x420"'
+""")
+osascript -e 'display dialog "Alright Hello my name is noneya and I have come to inform you that your computer is being held hostage. Please Message: Ferris622@icloud.com for help." buttons {"Continue"} default button "Continue" with icon caution with title "This is a joke ngl XD"'
+""")
+osascript -e 'display dialog "You've been pranked take the L sussy." buttons {"Continue"} default button "Continue" with icon caution with title "BRUH"'
+""")
+
+
+
+
+#Model Code:
+#os.system("""
+#osascript -e 'display dialog "Your computer has encountered an error." buttons {"Continue"} default button "Continue" with icon caution with title "error code 0x420"'
+#""")
+
+
+
+#Three Preloaded Icons are:
+#Caution
+#Note 
+#Stop
+
+
+#os.system("""
+#osascript -e 'Tell application "System Events" to display dialog "Your computer has encountered an error" with title "error"'
+#""")
+#display dialog "Your computer has encountered an error." buttons {"Don't Continue", "Continue"} default button "Continue" cancel button "Don't Continue"
